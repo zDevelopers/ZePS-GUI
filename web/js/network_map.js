074388e570e06c35c2b36f11e8ba3202a3e7ac86
Amaury Carrade <amaury@carrade.eu>
2016-04-04 15:51:00 +0200

    The network map is now constructed from an independent & configurable object.
    This makes the network map easily reusable to, as example, display a route on the map.

--- web/js/network_map.js
+++ web/js/network_map.js
@@ -1,233 +1,322 @@
-'use strict';
+(function(window) {
+    'use strict';
 
-/**
- * Converts Minecraft coordinates to lat/long used by Leaflet.
- *
- * The coordinates are divided by 1000, to be able to display points with Z>100, and inverted,
- * as the Minecraft coordinate system is different from the lat/long one.
- *
- * @param coordinates The coordinates, in a two-sized array [x, z].
- * @returns {*[]} The lat/long, in a two-sized array.
- */
-function coords2latlng(coordinates)
-{
-    return [-coordinates[1] / 1000, coordinates[0] / 1000];
-}
-
-
-/**
- * Checks if the given station is an intersection or a terminus.
- *
- * @param station The station.
- * @returns {{is_intersection: boolean, is_terminus: boolean}}
- */
-function get_neighborhood_infos(station)
-{
-    var relations_count = Object.keys(station.network).length;
-
-    return {
-        is_intersection: relations_count > 2,
-        is_terminus: relations_count <= 1
-    };
-}
-
-function add_station(name, location, color, is_intersection, is_terminus, is_main)
-{
-    var label_classes = [];
-    if (is_intersection || is_terminus || is_main)
-        label_classes.push('major_station');
-    if (is_terminus)
-        label_classes.push('terminus_station');
-    if (is_main)
-        label_classes.push('main_station');
-
-
-    return L.circleMarker(coords2latlng(location), {
-        color: is_terminus ? 'black' : color,
-        fillColor: is_intersection ? 'white' : (is_terminus ? 'black' : color),
-
-        opacity: 0.76,
-        fillOpacity: 1,
-
-        className: 'network-map-station major_station'
-    }).bindLabel('<div class="station-label' + (is_intersection || is_main ? ' station-label-intersection' : '') + '">' + name + '</div>', {
-        noHide: true,
-        className: label_classes.join(' ')
-    });
-}
-
-function add_line(from, to, color)
-{
-    return L.polyline([coords2latlng(from), coords2latlng(to)], {
-        color: color,
-        opacity: 1,
-        clickable: false,
-
-        className: 'network-map-line'
-    });
-}
-
-function link_stations(lines, network, station_base, direction, color)
-{
-    if (station_base.network[direction])
+    var NetworkMap =
     {
-        var station_other = network[station_base.network[direction]];
+        // The Leaflet map object
+        map: undefined,
 
-        if (station_other)
-            lines.push(add_line([station_base.x, station_base.y], [station_other.x, station_other.y], color));
-    }
-}
+        // The container ID
+        map_container_id: undefined,
 
+        // The layers displayed on the map
+        layer_main: undefined,
+        layer_intersections: undefined,
+        layer_terminus: undefined,
+        layer_others: undefined,
+        layer_lines: undefined,
 
-/**
- * Graceful adaptation to the zoom level.
- *
- * 12+:   all content displayed
- * 10-12: all dots, but labels of the main stations (intersections & main cities) only.
- * 10-:   only dots & labels of the main stations.
- *
- * @param map The Leaflet map object
- * @param layer_others The layer containing the dots of the less useful stations, hidden at low zooms.
- */
-function adapt_zoom(map, layer_others)
-{
-    var zoom_level = map.getZoom();
+        // The stations colors
+        stations_color_dot: 'purple',
+        stations_color_dot_intersection: 'white',
+        stations_color_dot_terminus: 'black',
+        stations_color_lines: 'purple',
 
-    var $labels          = $('.leaflet-label');
-    var $labels_major    = $('.leaflet-label.major_station');
-    var $labels_terminus = $('.leaflet-label.terminus_station');
+        // The main stations (array)
+        main_stations: [],
 
-    if (zoom_level >= 11)
-    {
-        $labels.show();
+        // The API to call to retrieve the JSON network
+        network_api: undefined,
 
-        if (!map.hasLayer(layer_others))
-            map.addLayer(layer_others);
-    }
-    else if (zoom_level == 10)
-    {
-        $labels.hide();
-        $labels_major.show();
-
-        if (!map.hasLayer(layer_others))
-            map.addLayer(layer_others);
-    }
-    else if (zoom_level == 9)
-    {
-        $labels.hide();
-        $labels_major.show();
-        $labels_terminus.hide();
-
-        map.removeLayer(layer_others);
-    }
-}
+        // The raw network object returned by the API but indexed by station ID.
+        network: {},
 
 
-
-// --------------------
-
-
-
-$(function ()
-{
-    var $network_map = $('#network_map');
-    var $still_nothing_loader = $('#still_nothing_loader');
-
-    $network_map.show();
-    $still_nothing_loader.hide();
-
-    // Displays the secondary loader a few seconds later, when the map should be loaded.
-    setTimeout(function () { $still_nothing_loader.show(); },4000);
-
-
-    var stations_color_dot = 'purple';
-    var stations_color_lines = 'purple';
-
-
-    $.getJSON(routes.stations_network, function(network)
-    {
-        // Constructs a map by ID
-        var network_by_id = {};
-        network.forEach(function(station)
+        /**
+         * Converts Minecraft coordinates to lat/long used by Leaflet.
+         *
+         * The coordinates are divided by 1000, to be able to display points with Z>100, and inverted,
+         * as the Minecraft coordinate system is different from the lat/long one.
+         *
+         * @param coordinates The coordinates, in a two-sized array [x, z].
+         * @returns {*[]} The lat/long, in a two-sized array.
+         * @private
+         */
+        _coords_to_latlng: function (coordinates)
         {
-            network_by_id[station.id] = station;
-        });
+            return [-coordinates[1] / 1000, coordinates[0] / 1000];
+        },
 
-
-        // Creates list of markers (will become layers later)
-        var markers_main          = [];
-        var markers_intersections = [];
-        var markers_terminus      = [];
-        var markers_others        = [];
-
-        var lines                 = [];
-
-
-        // Creates the stations, and the lines between them
-        network.forEach(function(station)
+        /**
+         * Checks if the given station is an intersection or a terminus.
+         *
+         * @param station The station.
+         * @returns {{is_intersection: boolean, is_terminus: boolean}}
+         * @private
+         */
+        _get_neighborhood_infos: function(station)
         {
-            if (station.is_visible)
+            var relations_count = Object.keys(station.network).length;
+
+            return {
+                is_intersection: relations_count > 2,
+                is_terminus: relations_count <= 1
+            };
+        },
+
+        /**
+         * Creates a station.
+         *
+         * @param name The station's display name.
+         * @param location The station's location (array: first key is X, other is Z coordinate in Minecraft system).
+         * @param color The station's color. May be overwritten if the station is a special one (see below).
+         * @param is_intersection True if this station is an intersection.
+         * @param is_terminus True if this station is a terminus.
+         * @param is_main True if this station is a main station.
+         * @returns {*} A Leaflet circleMarker object.
+         * @private
+         */
+        _create_station: function(name, location, color, is_intersection, is_terminus, is_main)
+        {
+            var label_classes = [];
+            if (is_intersection || is_terminus || is_main)
+                label_classes.push('major_station');
+            if (is_terminus)
+                label_classes.push('terminus_station');
+            if (is_main)
+                label_classes.push('main_station');
+
+
+            return L.circleMarker(NetworkMap._coords_to_latlng(location), {
+                color: is_terminus ? NetworkMap.stations_color_dot_terminus : color,
+                fillColor: is_intersection ? NetworkMap.stations_color_dot_intersection : (is_terminus ? NetworkMap.stations_color_dot_terminus : color),
+
+                opacity: 0.76,
+                fillOpacity: 1,
+
+                className: 'network-map-station'
+            }).bindLabel('<div class="station-label' + (is_intersection || is_main ? ' station-label-intersection' : '') + (is_main ? ' station-label-main' : '') + '">' + name + '</div>', {
+                noHide: true,
+                className: label_classes.join(' ')
+            });
+        },
+
+        /**
+         * Creates a line.
+         *
+         * @param from The line's first point (array: first key is X, other is Z coordinate in Minecraft system).
+         * @param to The line's last point.
+         * @param color The line's color.
+         * @return {*} A Leaflet polyline object.
+         * @private
+         */
+        _create_line: function(from, to, color)
+        {
+            return L.polyline([NetworkMap._coords_to_latlng(from), NetworkMap._coords_to_latlng(to)], {
+                color: color,
+                opacity: 1,
+                clickable: false,
+
+                className: 'network-map-line'
+            });
+        },
+
+        /**
+         * Links two stations with a line, if possible.
+         *
+         * @param lines The line will be added to this object, if it can be created.
+         * @param station_base The base station.
+         * @param direction The direction to use to find the other station. If a station is defined in this direction,
+         *                  a link will be created.
+         * @param color The line color.
+         * @private
+         */
+        _link_stations: function(lines, station_base, direction, color)
+        {
+            if (station_base.network[direction])
             {
-                var neighborhood = get_neighborhood_infos(station);
-                var is_main_station = main_stations.indexOf(station.code_name) > -1;
+                var station_other = NetworkMap.network[station_base.network[direction]];
 
-                var marker_station = add_station(
-                    station.full_name, [station.x, station.y], stations_color_dot,
-                    neighborhood.is_intersection, neighborhood.is_terminus, is_main_station
+                if (station_other)
+                    lines.push(NetworkMap._create_line(
+                        [station_base.x, station_base.y], [station_other.x, station_other.y],
+                        color
+                    ));
+            }
+        },
+
+        /**
+         * Graceful adaptation to the zoom level.
+         *
+         * 12+:   all content displayed
+         * 10-12: all dots, but labels of the main stations (intersections & main cities) only.
+         * 10-:   only dots & labels of the main stations.
+         *
+         * @private
+         */
+        _adapt_zoom: function()
+        {
+            var zoom_level = NetworkMap.map.getZoom();
+
+            var $labels = $('.leaflet-label');
+            var $labels_major = $('.leaflet-label.major_station');
+            var $labels_terminus = $('.leaflet-label.terminus_station');
+
+            if (zoom_level >= 11)
+            {
+                $labels.show();
+
+                if (!NetworkMap.map.hasLayer(NetworkMap.layer_others))
+                    NetworkMap.map.addLayer(NetworkMap.layer_others);
+            }
+            else if (zoom_level == 10)
+            {
+                $labels.hide();
+                $labels_major.show();
+
+                if (!NetworkMap.map.hasLayer(NetworkMap.layer_others))
+                    NetworkMap.map.addLayer(NetworkMap.layer_others);
+            }
+            else if (zoom_level == 9)
+            {
+                $labels.hide();
+                $labels_major.show();
+                $labels_terminus.hide();
+
+                NetworkMap.map.removeLayer(NetworkMap.layer_others);
+            }
+        },
+
+
+        // --------------------
+
+
+        /**
+         * Initializes the map.
+         */
+        init: function($still_nothing_loader, callback)
+        {
+            var $network_map = $('#' + NetworkMap.map_container_id);
+            $network_map.show();
+
+
+            if ($still_nothing_loader)
+            {
+                $still_nothing_loader.hide();
+
+                // Displays the secondary loader a few seconds later, when the map should be loaded.
+                setTimeout(function () {
+                    $still_nothing_loader.show();
+                }, 4000);
+            }
+
+
+            if (!NetworkMap.network_api)
+            {
+                console.error('NetworkMap: network API not configured.');
+                return;
+            }
+
+            if (!NetworkMap.map_container_id)
+            {
+                console.error('NetworkMap: container ID not configured.');
+                return;
+            }
+
+
+            $.getJSON(NetworkMap.network_api, function (network_array)
+            {
+                // Constructs a map by ID
+                network_array.forEach(function (station) {
+                    NetworkMap.network[station.id] = station;
+                });
+
+
+                // Creates list of markers (will become layers later)
+                var markers_main = [];
+                var markers_intersections = [];
+                var markers_terminus = [];
+                var markers_others = [];
+
+                var lines = [];
+
+
+                // Creates the stations, and the lines between them
+                network_array.forEach(function (station)
+                {
+                    if (station.is_visible)
+                    {
+                        var neighborhood = NetworkMap._get_neighborhood_infos(station);
+                        var is_main_station = NetworkMap.main_stations.indexOf(station.code_name) > -1;
+
+                        var marker_station = NetworkMap._create_station(
+                            station.full_name, [station.x, station.y], NetworkMap.stations_color_dot,
+                            neighborhood.is_intersection, neighborhood.is_terminus, is_main_station
+                        );
+
+                        if (is_main_station)              markers_main.push(marker_station);
+                        else if (neighborhood.is_intersection) markers_intersections.push(marker_station);
+                        else if (neighborhood.is_terminus)     markers_terminus.push(marker_station);
+                        else                                   markers_others.push(marker_station);
+                    }
+
+                    if (station.network)
+                    {
+                        NetworkMap._link_stations(lines, station, 'east',  NetworkMap.stations_color_lines);
+                        NetworkMap._link_stations(lines, station, 'north', NetworkMap.stations_color_lines);
+                        NetworkMap._link_stations(lines, station, 'south', NetworkMap.stations_color_lines);
+                        NetworkMap._link_stations(lines, station, 'west',  NetworkMap.stations_color_lines);
+                    }
+                });
+
+
+                // Creates the layers, and display them on the map
+                NetworkMap.layer_main          = L.layerGroup(markers_main);
+                NetworkMap.layer_intersections = L.layerGroup(markers_intersections);
+                NetworkMap.layer_terminus      = L.layerGroup(markers_terminus);
+                NetworkMap.layer_others        = L.layerGroup(markers_others);
+                NetworkMap.layer_lines         = L.layerGroup(lines);
+
+
+                // Removes the loader
+                $network_map.empty();
+
+
+                // Loads the map
+                NetworkMap.map = L.map(NetworkMap.map_container_id, {
+                    center: [0, 0],
+                    zoom: 10,
+
+                    minZoom: 9,
+                    maxZoom: 14,
+
+                    // Layers are added by inverted order of importance—the last will be displayed on top.
+                    layers: [
+                        NetworkMap.layer_lines,
+
+                        NetworkMap.layer_others,
+                        NetworkMap.layer_terminus,
+                        NetworkMap.layer_intersections,
+                        NetworkMap.layer_main
+                    ]
+                });
+
+                NetworkMap.map.attributionControl.addAttribution(
+                    'Plan du Netherrail <a href="https://zcraft.fr">Zcraftien</a> | Données aggrégées par <a href="https://github.com/FlorianCassayre/ZePS-Core">Florian Cassayre</a>'
                 );
 
-                if      (is_main_station)              markers_main.push(marker_station);
-                else if (neighborhood.is_intersection) markers_intersections.push(marker_station);
-                else if (neighborhood.is_terminus)     markers_terminus.push(marker_station);
-                else                                   markers_others.push(marker_station);
-            }
 
-            if (station.network)
-            {
-                link_stations(lines, network_by_id, station, 'east', stations_color_lines);
-                link_stations(lines, network_by_id, station, 'north', stations_color_lines);
-                link_stations(lines, network_by_id, station, 'south', stations_color_lines);
-                link_stations(lines, network_by_id, station, 'west', stations_color_lines);
-            }
-        });
+                // Ensures the zoom is handled correctly
+                NetworkMap._adapt_zoom();
+                NetworkMap.map.on('zoomend', function () { NetworkMap._adapt_zoom(); });
 
 
-        // Creates the layers, and display them on the map
-        var layer_main          = L.layerGroup(markers_main);
-        var layer_intersections = L.layerGroup(markers_intersections);
-        var layer_terminus      = L.layerGroup(markers_terminus);
-        var layer_others        = L.layerGroup(markers_others);
-        var layer_lines         = L.layerGroup(lines);
+                // Callback
+                if (callback)
+                    callback(NetworkMap);
+            });
+        }
+    };
 
-
-        // Removes the loader
-        $network_map.empty();
-
-
-        // Loads the map
-        var map = L.map('network_map', {
-            center: [0, 0],
-            zoom: 10,
-
-            minZoom: 9,
-            maxZoom: 14,
-
-            // Layers are added by inverted order of importance—the last will be displayed on top.
-            layers: [
-                layer_lines,
-
-                layer_others,
-                layer_terminus,
-                layer_intersections,
-                layer_main
-            ]
-        });
-
-        map.attributionControl.addAttribution('Plan du Netherrail <a href="https://zcraft.fr">Zcraftien</a> | Données aggrégées par <a href="https://github.com/FlorianCassayre/ZePS-Core">Florian Cassayre</a>');
-
-
-        // Ensures the zoom is handled correctly
-        adapt_zoom(map, layer_others);
-        map.on('zoomend', function() { adapt_zoom(map, layer_others); });
-    });
-});
+    window.NetworkMap = NetworkMap;
+})(window);
