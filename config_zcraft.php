b7b70f755e76e8e4e79f003b2c0bf9ff64e787dd
Amaury Carrade <amaury@carrade.eu>
2018-03-30 00:30:02 +0200

    Added partners on about page. * NEW: added partners displayed on the “about” page, with logo & description. The data is on the configuration file, default empty. * NEW: added a “zcraft” config file containing values for Zcraft, so they can be versionned, and logos for easier deployment (only used for Zcraft).

---
+++ config_zcraft.php
@@ -0,0 +1,23 @@
+<?php
+return [
+	'roleplay' => [
+        'partners' => [
+            [
+                'name' => 'Trainline',
+                'name_displayed' => false,
+                'logo' => '/img/zcraft/trainline.svg',
+                'description' => 'En tant qu\'outil développé essentiellement dans le train, le ZéPS salue le soutien précieux de Trainline, son fournisseur officiel de billets et d\'espaces de travail à grande vitesse.',
+                'link' => 'https://trainline.fr',
+                'tooltip' => '(Bon ok, en vrai on a pas de partenariat officiel. Mais le reste reste vrai !)'
+            ],
+            [
+                'name' => 'Société Zcraftienne des Transports (SZT)',
+                'name_displayed' => false,
+                'logo' => '/img/zcraft/szt.png',
+                'logo_css_classes' => 'is-tall is-lighter',
+                'description' => 'La Société Zcraftienne des Transports est un acteur majeur du réseau Zcraftien. Nous sommes fiers d\'agir à ses côtés pour vous aider à toujours avoir un train d\'avance.',
+                'link' => 'https://forum.zcraft.fr/viewtopic.php?id=6742'
+            ]
+        ]
+    ]
+];
